17d4662cd67a2a1647782c479070f3f138594420
unknown <sjxliu8@gmail.com>
2021-12-11 16:27:07 -0600

    home images showing, too big tho

--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -1,12 +1,18 @@
 const React = require("react")
 const Default = require("../Default")
 
-function Show (){
+function Show (data){
 
     return(
         <Default>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.places.name}</h1>
+                <section>
+                    <p>Currently Unrated</p>
+                </section>
+                <section>
+                    <p>No Comments Yet</p>
+                </section>
             </main>
         </Default>
     )
