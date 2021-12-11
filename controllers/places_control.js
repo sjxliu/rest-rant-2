8aeb6c7d130f0137ec1ce30c703e28b44bd99024
unknown <sjxliu8@gmail.com>
2021-12-11 09:16:53 -0600

    show routes connected

--- controllers/places_control.js
+++ controllers/places_control.js
@@ -31,4 +31,19 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
   })
 
+
+router.get("/:id", (req, res)=>{
+  res.render("places/show")
+  let id = Number(req.params.id)
+  if (isNaN(id)){
+    res.render("error404")
+  } else if (!places[id]) {
+res.render("error404")
+  }
+   else {
+    res.render("places/show", {places:places[id]})
+  }
+})
+
+
 module.exports = router
---
+++ views/places/Show.jsx
@@ -0,0 +1,16 @@
+const React = require("react")
+const Default = require("../Default")
+
+function Show (){
+
+    return(
+        <Default>
+            <main>
+                <h1>Show Page</h1>
+            </main>
+        </Default>
+    )
+
+}
+
+module.exports = Show;
